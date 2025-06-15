29e66e91ed38ddb154bdba034ffdf5af53984f01
Zorishimo <caseyj77@gmail.com>
2025-06-15 20:00:14 +0000

    removed paragraph tag from HomeView

--- habits/src/views/HomeView.vue
+++ habits/src/views/HomeView.vue
@@ -8,8 +8,7 @@ import AddHabitForm from '@/components/AddHabitForm.vue'
 <template>
   <div class="p-4">
     <h1 class="text-2xl font-bold">Habit Tracker</h1>
-    <p>Welcome! Ready to build better habits?</p>
-
+    
     <HabitList />
     <AddHabitForm />
   </div>
